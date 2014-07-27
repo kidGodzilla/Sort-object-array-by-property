f2e0f93a7a9fed5e62818aef6e37608db6813ccf
James Futhey <kidGodzilla@users.noreply.github.com>
2014-07-26 20:28:13 -0700

    Update sortJSON.js

--- sortJSON.js
+++ sortJSON.js
@@ -1,7 +1,7 @@
 function sortJSON(key,array,dir) {
     /* Sorts an array of objects by an object key */
+    dir = dir || 1;
     array.sort(function(h, i) {
-        dir = dir || 1;
         return h[key].toLowerCase() < i[key].toLowerCase() ? -1 * dir : (h[key].toLowerCase() > i[key].toLowerCase() ? 1 * dir : 0);
     });
 }
